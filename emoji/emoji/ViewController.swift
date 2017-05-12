50392c4eb73b02725f55cc5852477ccb691c7d5b
4n4rk0z <4n4rk0z@R41d.local>
2017-05-12 13:22:55 -0500

    demo de emojis

---
+++ emoji/emoji/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emoji
+//
+//  Created by 4n4rk0z on 5/12/17.
+//  Copyright © 2017 com.beaconsDemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ emoji/emoji/demo.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let pablo = "💩"
+
+
+print("mi amigo pablo es \(pablo)")
+
